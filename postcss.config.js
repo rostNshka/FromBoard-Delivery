0ea2b476b7341f4e1837e2c181305f69a1853df7
RostNshka <devkotder@ya.ru>
2025-12-03 00:40:07 +0400

    add library postcss-pxtorem and setting it

---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+import postcssPxToRem from 'postcss-pxtorem'
+
+export default ({ env }) => {
+  const isProd = env === 'production'
+  const plugins = []
+
+  if(isProd) {
+    plugins.push(
+      postcssPxToRem({
+        propList: ['*'],
+        mediaQueries: true,
+      })
+    )
+  }
+
+  return {
+    plugins,
+  }
+}
