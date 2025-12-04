34030692b53b878313359030a50ce6c9b41531a6
RostNshka <devkotder@ya.ru>
2025-12-05 02:35:44 +0400

    add navigation menu

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,5 +1,15 @@
+import { NavLink } from 'react-router-dom'
+
 const Header = () => {
-  return <header>HEADER</header>
+  return (
+    <header className="header">
+      <NavLink to={'/'}>Главная</NavLink>
+      <NavLink to={'/calculator'}>Калькулятор</NavLink>
+      <NavLink to={'/guarantees'}>Гарантии</NavLink>
+      <NavLink to={'/reviews'}>Отзывы</NavLink>
+      <NavLink to={'/feedback'}>Задать вопрос</NavLink>
+    </header>
+  )
 }
 
 export default Header
