4cd5d49ea25b2f1b078109a456798fe8bf173885
RostNshka <devkotder@ya.ru>
2025-12-07 19:38:18 +0400

    Footer all styles and all markup

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,5 +1,16 @@
+import Logotype from '@/components/Logotype'
+import Navigation from '@/components/Navigation'
+import Addition from '@/components/Addition'
+import './Footer.scss'
+
 const Footer = () => {
-  return <footer>HEADER</footer>
+  return (
+    <footer className="footer">
+      <Logotype color = 'light'/>
+      <Navigation color = 'light'/>
+      <Addition color = 'light'/>
+    </footer>
+  )
 }
 
 export default Footer
