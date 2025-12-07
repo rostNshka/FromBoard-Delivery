5813f57865f4d46c39e97e02e037f2d207067b0d
RostNshka <devkotder@ya.ru>
2025-12-07 19:38:08 +0400

    Header all styles and all markup

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,13 +1,19 @@
 import Logotype from '@/components/Logotype'
 import './Header.scss'
 import Navigation from '@/components/Navigation'
+import Addition from '@/components/Addition'
+import Button from '@/components/Button'
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className="header container">
       <div className="header__main">
         <Logotype color='dark' />
         <Navigation color='dark' />
+      </div>
+      <div className='header__info'>
+        <Addition>Доставляем товары из заграницы в Россию</Addition>
+        <Button>Оставить заявку</Button>
       </div>
     </header>
   )
