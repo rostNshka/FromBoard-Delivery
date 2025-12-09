16f7c01f5a61a60c88a7938a52cdc8ae0970ffb7
RostNshka <devkotder@ya.ru>
2025-12-09 21:00:48 +0400

    add data for Guarantees

---
+++ src/utils/guaranteesData.js
@@ -0,0 +1,18 @@
+export const guaranteesData = [
+  {
+    imgSrc: '../../public/guarantees/1.jpg',
+    title: 'Свидетельство о регистрации в РФ',
+  },
+  {
+    imgSrc: '../../public/guarantees/2.jpg',
+    title: 'Свидетельство о регистрации в США',
+  },
+  {
+    imgSrc: '../../public/guarantees/3.jpg',
+    title: 'Свидетельство о регистрации в Китае',
+  },
+  {
+    imgSrc: '../../public/guarantees/4.jpg',
+    title: 'Свидетельство о регистрации в Англии',
+  },
+  ]
