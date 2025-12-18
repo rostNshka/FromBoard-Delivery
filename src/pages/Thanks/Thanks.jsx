cee30f1076a7b912680364d7c9d29590edae1bd8
RostNshka <devkotder@ya.ru>
2025-12-18 17:57:32 +0400

    add new page and logic

---
+++ src/pages/Thanks/Thanks.jsx
@@ -0,0 +1,23 @@
+import './Thanks.scss'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+
+const Thanks = () => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/', { replace: true })
+    }, 3000)
+
+    return () => clearTimeout(timer)
+  }, [navigate])
+
+  return (
+    <div className="thanks container">
+      <p>Спасибо за обращение! <br/> Перенаправляем на главную странницу!</p>
+    </div>
+  )
+}
+
+export default Thanks
